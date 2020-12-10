a968b15c473bcbd2e59863ba67ab555702355a5b
wonderlexicon <nh756@nyu.edu>
2020-12-10 18:29:50 -0500

    added audio preload function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-adist/aframe-multisrc-component.js
+
 
 console.log("me");
 
@@ -24,47 +24,22 @@ window.addEventListener("wheel", event=>{
     }
 
     myCamera.setAttribute("zoom", newZoom);
-    console.log({newZoom})
+
 })
 
-
-
-
-AFRAME.registerShader('grid-glitch', {
-  schema: {
-    color: {type: 'color', is: 'uniform'},
-    timeMsec: {type: 'time', is: 'uniform'}
-  },
-​
-  vertexShader: `
-varying vec2 vUv;
-​
-void main() {
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}
-`,
-  fragmentShader: `
-varying vec2 vUv;
-uniform vec3 color;
-uniform float timeMsec; // A-Frame time in milliseconds.
-​
-void main() {
-  float time = timeMsec / 1000.0; // Convert from A-Frame milliseconds to typical time in seconds.
-  // Use sin(time), which curves between 0 and 1 over time,
-  // to determine the mix of two colors:
-  //    (a) Dynamic color where 'R' and 'B' channels come
-  //        from a modulus of the UV coordinates.
-  //    (b) Base color.
-  // 
-  // The color itself is a vec4 containing RGBA values 0-1.
-  gl_FragColor = mix(
-    vec4(mod(vUv , 0.05) * 20.0, 1.0, 1.0),
-    vec4(color, 1.0),
-    sin(time)
-  );
-}
-`
+// Component to change to a sequential color on click.
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    var lastIndex = -1;
+   
+    this.el.addEventListener('click', function (evt) {
+      lastIndex = (lastIndex + 1) % COLORS.length;
+      this.setAttribute('material', 'color', COLORS[lastIndex]);
+      console.log('I was clicked at: ', evt.detail.intersection.point);
+    });
+  }
 });
-​
+
+
+
 
